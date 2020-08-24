cd97d96878f13b28237134e888c119a79175f91e
soorajshankar <soorajshankar@gmail.com>
2020-08-24 17:46:07 +0530

    test

--- app/cli/src/commands/query.ts
+++ app/cli/src/commands/query.ts
@@ -1,62 +1,83 @@
-import * as path from 'path'
-import * as fs from 'fs-extra'
-import * as yaml from 'js-yaml'
+import * as path from "path";
+import * as fs from "fs-extra";
+import * as yaml from "js-yaml";
+import shell from "shelljs";
 
-import { Command, flags } from '@oclif/command'
-import { BenchmarkRunner } from '../../../queries/src/main'
-import { isHttpUrl, promisifiedRequest } from '../../../queries/src/utils'
-import type { GlobalConfig } from '../../../queries/src/executors/base/types'
+import { Command, flags } from "@oclif/command";
+import { BenchmarkRunner } from "../../../queries/src/main";
+import { isHttpUrl, promisifiedRequest } from "../../../queries/src/utils";
+import type { GlobalConfig } from "../../../queries/src/executors/base/types";
 
 export default class Query extends Command {
-  static description = 'benchmark queries or mutations'
+  static description = "benchmark queries or mutations";
 
   static examples = [
     `$ graphql-bench query --config ./config.query.yaml --outfile results.json`,
-  ]
+  ];
 
   static flags = {
-    help: flags.help({ char: 'h' }),
+    help: flags.help({ char: "h" }),
     config: flags.string({
-      char: 'c',
+      char: "c",
       required: true,
       multiple: false,
-      description: 'Filepath to YAML config file for query benchmarks or URL of the YAML config file',
+      description:
+        "Filepath to YAML config file for query benchmarks or URL of the YAML config file",
       parse: (filepath) => {
         if (isHttpUrl(filepath)) {
           // async is not supported here, processing url in the `run` method
           return filepath;
         }
-        const pathToFile = path.join(process.cwd(), filepath)
-        const configFile = fs.readFileSync(pathToFile, 'utf-8')
-        return yaml.load(configFile)
+        const pathToFile = path.join(process.cwd(), filepath);
+        const configFile = fs.readFileSync(pathToFile, "utf-8");
+        return yaml.load(configFile);
       },
     }),
     outfile: flags.string({
-      char: 'o',
+      char: "o",
       required: false,
       multiple: false,
-      description: 'Filepath to output JSON file containing benchmark stats',
+      description: "Filepath to output JSON file containing benchmark stats",
     }),
-  }
+  };
 
   async run() {
-    const { flags } = this.parse(Query)
-    if (typeof flags.config === 'string' && isHttpUrl(flags.config)) {
-      let response = await promisifiedRequest(flags.config)
-      flags.config = yaml.load(response)
-    }
+    const { flags } = this.parse(Query);
+    // if (typeof flags.config === "string" && isHttpUrl(flags.config)) {
+    //   let response = await promisifiedRequest(flags.config);
+    //   flags.config = yaml.load(response);
+    // }
 
-    // Oclif, can't figure out how to generically type flags =/
-    const executor = new BenchmarkRunner(
-      (flags.config as unknown) as GlobalConfig
-    )
-    const results = await executor.runBenchmarks()
+    // // Oclif, can't figure out how to generically type flags =/
+    // const executor = new BenchmarkRunner(
+    //   (flags.config as unknown) as GlobalConfig
+    // );
+    // const results = await executor.runBenchmarks();
 
-    if (flags.outfile) {
-      const pathToOutfile = path.join(process.cwd(), flags.outfile)
-      fs.outputJSONSync(pathToOutfile, results, {
-        spaces: 2,
-      })
+    // if (flags.outfile) {
+    //   const pathToOutfile = path.join(process.cwd(), flags.outfile);
+    //   fs.outputJSONSync(pathToOutfile, results, {
+    //     spaces: 2,
+    //   });
+    // }
+
+    if (!shell.which("git")) {
+      shell.echo("Sorry, this script requires git");
+      shell.exit(1);
     }
+    shell.exec(
+      "cd ..\
+      && git clone https://github.com/soorajshankar/auto-test.git"
+    );
+
+    shell.cp("-R", "../queries/reports", "../auto-test/reports");
+    shell.exec(
+      'cd .. \
+      cd auto-test \
+      && git commit -am "test" \
+      && git push\
+      && echo "test"'
+    );
+    shell.exec('ls')
   }
 }
